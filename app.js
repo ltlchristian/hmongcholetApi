7a695be351d67ab97dabf588d3e80da96dae8602
Christian ly <christian_ly@outlook.fr>
2023-01-30 23:04:02 +0100

    test cron

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const { importProduct } = require("./utils/utils");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+var CronJob = require("cron").CronJob;
+var job1 = new CronJob(
+  "* * * * * *",
+  function () {
+    console.log("You will see this message every second");
+    importProduct();
+  },
+  null,
+  false,
+  "America/Los_Angeles"
+);
+// Use this if the 4th param is default value(false)
+//job1.start();
+
+var job2 = new CronJob(
+  "* * * * * *",
+  function () {
+    console.log("You will see this another message every second");
+  },
+  null,
+  false,
+  "America/Los_Angeles"
+);
+// Use this if the 4th param is default value(false)
+//job2.start();
+
+/*setTimeout(function () {
+  console.log("****** STOP JOBS *********");
+  job1.stop();
+  job2.stop();
+}, 5000);*/
 
 module.exports = app;
--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,13 @@
-var express = require("express");
-var router = express.Router();
 const fs = require("fs");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
+const importProduct = () => {
   const fichier =
     "/media/cly/Disque1T/GIT-WORDPRESS/hmongcholetApi/data/test1.csv";
   //const fichier = "/home/kzsm4017/transfert/entree/test1.csv";
   const data = fs.readFileSync(fichier, "utf8");
   console.log("===> testcly :: data=", data);
-  res.send(data);
-});
+};
 
-module.exports = router;
+module.exports = {
+  importProduct,
+};
