4ba3c5457d5c4ad4fe1e486c52c3f7df8b6f9ed3
Christian ly <christian_ly@outlook.fr>
2023-01-30 21:45:23 +0100

    test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  //const fichier = "/media/cly/Disque1T/GIT-WORDPRESS/hmongcholetApi/data/test1.csv";
+  const fichier = "/home/kzsm4017/transfert/entree/test1.csv";
+  const data = fs.readFileSync(fichier, "utf8");
+  console.log("===> testcly :: data=", data);
+  res.send(data);
 });
 
 module.exports = router;
